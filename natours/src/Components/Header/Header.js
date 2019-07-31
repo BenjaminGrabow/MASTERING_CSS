afc8b2072c5d506c41e58fb0e5f1c6727ddc1ecc
Benjamin Grabow <benjamin_grabow@gmx.de>
2019-07-31 22:18:44 +0200

    Feature<:

--- natours/src/Components/Header/Header.js
+++ natours/src/Components/Header/Header.js
@@ -4,18 +4,18 @@ import './Header.scss';
 
 const Header = () => {
   return (
-    <header>
-      <div className="logo-box">
-        <img className="logo" src={Logo} alt="Logo" />
+    <header className="header">
+      <div className="header__logo-box">
+        <img className="header__logo" src={Logo} alt="Logo" />
       </div>
 
-      <div className="text-box">
+      <div className="header__text-box">
         <h1 className="heading-primary">
-          <span className="heading-primary-main">Outdoors</span>
-          <span className="heading-primary-sub">is where life happens</span>
+          <span className="heading-primary--main">Outdoors</span>
+          <span className="heading-primary--sub">is where life happens</span>
         </h1>
 
-        <a href="#" className="btn btn-white btn-animated">Discover our Tours</a>
+        <a href="#" className="btn btn--white btn--animated">Discover our Tours</a>
       </div>
     </header>
 
