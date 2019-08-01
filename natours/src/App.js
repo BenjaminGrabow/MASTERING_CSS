dd7ba8bd1d2efa3e6f1a743227a135483abbb64c
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-01 20:05:03 +0200

    Feature: added navigation to app and started styling

--- natours/src/App.js
+++ natours/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Components/Header/Header';
+import Nav from './Components/Navigation/Navigation';
 import './App.css';
 
 function App() {
   return (
     <div>
      <Header/>
+     <Nav/>
     </div>
   );
 }
